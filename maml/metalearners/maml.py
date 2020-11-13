Added ensemble method and fixed backpropagation through input to transformer

maml/metalearners/maml.py
@@ -8,6 +8,7 @@ from collections import OrderedDict
 from torchmeta.utils import gradient_update_parameters
 from maml.utils import tensors_to_device, compute_accuracy, gradient_update_parameters_warp
 from maml.model import ExpandingParameter
+from ..model import BatchLinear
 
 __all__ = ['ModelAgnosticMetaLearning', 'MAML', 'FOMAML']
 
@@ -54,19 +55,23 @@ class ModelAgnosticMetaLearning(object):
            International Conference on Learning Representations (ICLR).
            (https://arxiv.org/abs/1810.09502)
     """
-    def __init__(self, model, optimizer=None, warp_model=None, step_size=0.1, first_order=False,
+    def __init__(self, model, optimizer=None, warp_optimizer=None, warp_model=None, step_size=0.1, first_order=False,
                  learn_step_size=False, per_param_step_size=False,
-                 num_adaptation_steps=1, scheduler=None,
-                 loss_function=F.cross_entropy, device=None):
+                 num_adaptation_steps=1, scheduler=None, warp_scheduler=None,
+                 loss_function=F.cross_entropy, device=None, num_maml_steps=0):
         self.model = model.to(device=device)
         self.warp_model = warp_model.to(device=device) if warp_model is not None else None
         self.optimizer = optimizer
+        self.warp_optimizer = warp_optimizer
         self.step_size = step_size
         self.first_order = first_order
         self.num_adaptation_steps = num_adaptation_steps
         self.scheduler = scheduler
+        self.warp_scheduler = warp_scheduler
         self.loss_function = loss_function
         self.device = device
+        self.num_maml_steps = num_maml_steps
+        self.num_steps = 0
 
         if per_param_step_size:
             self.step_size = OrderedDict((name, torch.tensor(step_size,
@@ -86,7 +91,7 @@ class ModelAgnosticMetaLearning(object):
                 self.scheduler.base_lrs([group['initial_lr']
                     for group in self.optimizer.param_groups])
 
-    def get_outer_loss(self, batch):
+    def get_outer_loss(self, batch, eval_mode=False):
         if 'test' not in batch:
             raise RuntimeError('The batch does not contain any test dataset.')
 
@@ -113,27 +118,54 @@ class ModelAgnosticMetaLearning(object):
         test_inputs = batch["test"][0]
         test_targets = batch["test"][1]
 
-        params, adaptation_results = self.adapt(train_inputs, train_targets,
-            is_classification_task=is_classification_task,
-            num_adaptation_steps=self.num_adaptation_steps,
-            step_size=self.step_size, first_order=self.first_order)
+        if eval_mode:
+            for i in range(train_inputs.size(0)):
+                train_input_exp = train_inputs[i].unsqueeze(0).expand(train_inputs.size(0), *list(train_inputs.size())[1:]).contiguous()
+                train_target_exp = train_targets[i].unsqueeze(0).expand(train_targets.size(0), *list(train_targets.size())[1:]).contiguous()
+                test_input_exp = test_inputs[i].unsqueeze(0).expand(test_inputs.size(0), *list(test_inputs.size())[1:]).contiguous()
 
-        #results['inner_losses'][:] = adaptation_results['inner_losses']
-        if is_classification_task:
-            results['accuracies_before'] = adaptation_results['accuracy_before']
+                params, adaptation_results = self.adapt(train_input_exp, train_target_exp,
+                    is_classification_task=is_classification_task,
+                    num_adaptation_steps=self.num_adaptation_steps,
+                    step_size=self.step_size, first_order=self.first_order)
 
-        with torch.set_grad_enabled(self.model.training):
-            test_logits = self.model(test_inputs, params=params)
-            outer_loss = self.loss_function(test_logits, test_targets)
-            results['outer_losses'] = outer_loss.item()
-            mean_outer_loss += outer_loss
+                with torch.set_grad_enabled(False):
+                    test_logits = self.model(test_input_exp, params=params)
+                    softmax = torch.nn.functional.softmax(test_logits, dim=-1)
+                    softmax = softmax.mean(dim=0)
+                    test_logits = torch.log(softmax)
+                    outer_loss = self.loss_function(test_logits, test_targets[i])
+                    mean_outer_loss += outer_loss
 
-        if is_classification_task:
-            results['accuracies_after'] = compute_accuracy(
-                test_logits, test_targets)
+                if is_classification_task:
+                    results['accuracies_after'][i] = compute_accuracy(
+                        test_logits, test_targets[i])
 
-        mean_outer_loss.div_(num_tasks)
-        results['mean_outer_loss'] = mean_outer_loss.item()
+                torch.cuda.empty_cache()
+
+            mean_outer_loss.div_(num_tasks)
+            results['mean_outer_loss'] = mean_outer_loss.item()
+        else:
+            params, adaptation_results = self.adapt(train_inputs, train_targets,
+                is_classification_task=is_classification_task,
+                num_adaptation_steps=self.num_adaptation_steps,
+                step_size=self.step_size, first_order=self.first_order)
+
+            #results['inner_losses'][:] = adaptation_results['inner_losses']
+            if is_classification_task:
+                results['accuracies_before'] = adaptation_results['accuracy_before']
+
+            with torch.set_grad_enabled(self.model.training):
+                test_logits = self.model(test_inputs, params=params)
+                outer_loss = self.loss_function(test_logits, test_targets)
+                mean_outer_loss += outer_loss
+
+            if is_classification_task:
+                results['accuracies_after'] = compute_accuracy(
+                    test_logits, test_targets)
+
+            mean_outer_loss.div_(num_tasks)
+            results['mean_outer_loss'] = mean_outer_loss.item()
 
         return mean_outer_loss, results
 
@@ -150,6 +182,7 @@ class ModelAgnosticMetaLearning(object):
 
         results = {}
 
+        state = None
         for step in range(num_adaptation_steps):
             if self.warp_model is not None:
                 self.warp_model.set_listening(True)
@@ -166,9 +199,9 @@ class ModelAgnosticMetaLearning(object):
                 results['accuracy_before'] = compute_accuracy(logits, targets)
 
             self.model.zero_grad()
-            params = gradient_update_parameters_warp(self.model, inner_loss,
+            params, state = gradient_update_parameters_warp(self.model, inner_loss,
                 warp_model=self.warp_model, step_size=step_size, params=params,
-                first_order=(not self.model.training) or first_order)
+                first_order=(not self.model.training) or first_order, state=state)
 
             if self.warp_model is not None:
                 self.warp_model.set_listening(False)
@@ -179,6 +212,7 @@ class ModelAgnosticMetaLearning(object):
         with tqdm(total=max_batches, disable=not verbose, **kwargs) as pbar:
             for results in self.train_iter(dataloader, max_batches=max_batches):
                 pbar.update(1)
+                self.num_steps = 0
                 postfix = {'loss': '{0:.4f}'.format(results['mean_outer_loss'])}
                 wandb_data = {'outer_loss': results['mean_outer_loss']}
                 if 'accuracies_after' in results:
@@ -197,22 +231,52 @@ class ModelAgnosticMetaLearning(object):
                 'parameters(), lr=0.01), ...).'.format(__class__.__name__))
         num_batches = 0
         self.model.train()
+        if self.warp_model is not None:
+            self.warp_model.train()
         while num_batches < max_batches:
             for batch in dataloader:
                 if num_batches >= max_batches:
                     break
 
-                if self.scheduler is not None:
-                    self.scheduler.step(epoch=num_batches)
+                self.num_steps += 1
 
                 self.optimizer.zero_grad()
+                if self.warp_optimizer is not None:
+                    self.warp_optimizer.zero_grad()
 
                 batch = tensors_to_device(batch, device=self.device)
                 outer_loss, results = self.get_outer_loss(batch)
                 yield results
 
                 outer_loss.backward()
-                self.optimizer.step()
+
+
+                if self.num_maml_steps <= 0 or self.num_steps < self.num_maml_steps:
+                    self.optimizer.step()
+
+                if self.warp_optimizer is not None:
+                    self.warp_optimizer.step()
+
+                for param_group in self.optimizer.param_groups:
+                    wandb.log({"maml_lr": param_group['lr']}, commit=False)
+                    break
+
+                if self.warp_optimizer is not None:
+                    for param_group in self.warp_optimizer.param_groups:
+                        wandb.log({"warp_lr": param_group['lr']}, commit=False)
+                        break
+
+                if self.scheduler is not None:
+                    self.scheduler.step()
+
+                if self.warp_scheduler is not None:
+                    self.warp_scheduler.step()
+
+                """
+                for module in self.model.modules():
+                    if isinstance(module, BatchLinear):
+                        module.reset_parameters()
+                """
 
                 num_batches += 1
 
@@ -235,18 +299,26 @@ class ModelAgnosticMetaLearning(object):
         if 'accuracies_after' in results:
             mean_results['accuracies_after'] = mean_accuracy
 
+        wandb_data = {
+            "mean_outer_loss_eval": mean_outer_loss,
+            "mean_accuracy_eval": mean_accuracy
+        }
+        wandb.log(wandb_data, commit=False)
+
         return mean_results
 
     def evaluate_iter(self, dataloader, max_batches=500):
         num_batches = 0
         self.model.eval()
+        if self.warp_model is not None:
+            self.warp_model.eval()
         while num_batches < max_batches:
             for batch in dataloader:
                 if num_batches >= max_batches:
                     break
 
                 batch = tensors_to_device(batch, device=self.device)
-                _, results = self.get_outer_loss(batch)
+                _, results = self.get_outer_loss(batch, eval_mode=True)
                 yield results
 
                 num_batches += 1

maml/model.py
@@ -8,21 +8,95 @@ from collections import OrderedDict
 from torchmeta.modules import (MetaModule, MetaConv2d, MetaBatchNorm2d,
                                MetaSequential, MetaLinear)
 
-class ExpandingParameter(nn.Parameter):
-    def __new__(cls, data=None, requires_grad=True):
+class BatchParameter(nn.Parameter):
+    def __new__(cls, data=None, convolutional=False, in_size=0, in_channels=0, requires_grad=True):
         if data is None:
             data = torch.Tensor()
 
         instance = torch.Tensor._make_subclass(cls, data, requires_grad)
         instance.input_data = None
         instance.grad_data = None
+        instance.convolutional = convolutional
         instance.listening = False
+        instance.in_channels = in_channels
+        instance.in_size = in_size
+        return instance
+
+class BatchLinear(MetaModule):
+
+    def __init__(self, input_size, output_size, meta_batch_size=1, bias=True):
+        super(BatchLinear, self).__init__()
+
+        self.input_size = input_size
+        self.output_size = output_size
+        self.bias = bias
+        if bias:
+            input_size += 1
+        self.weight = BatchParameter(torch.Tensor(meta_batch_size, self.output_size,
+                                                 input_size))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        init.kaiming_uniform_(self.weight[:, :, :-1], a=math.sqrt(5))
+        if self.bias:
+            fan_in, _ = init._calculate_fan_in_and_fan_out(
+                self.weight[:, :, :-1])
+            bound = 1 / math.sqrt(fan_in)
+            init.uniform_(self.weight[:, :, -1], -bound, bound)
+
+    def forward(self, input_data, params=None, save_warp_data=True):
+        weight = self.weight if params is None else params["weight"]
+
+        if self.bias:
+            input_data = torch.cat([input_data,
+                torch.ones(*input_data.size()[:-1], 1, device=input_data.device)], -1)
+
+        if input_data.ndim > 3:
+            input_data_resized = input_data.view(input_data.size(0), -1, input_data.size(-1))
+            out = torch.transpose(torch.bmm(weight, torch.transpose(input_data_resized, 1,
+                                                                        2)), 1, 2)
+            out = out.reshape(*(input_data.size()[:-1] + out.size()[-1:]))
+
+        else:
+            out = torch.transpose(torch.bmm(weight, torch.transpose(input_data, 1,
+                                                                        2)), 1, 2)
+        if save_warp_data:
+            self.enable_warp_data(input_data, out)
+
+        return out
+
+    def enable_warp_data(self, input_data, out):
+        if self.weight.listening:
+            # Save input data for future reference
+            self.weight.input_data = input_data
+            self.weight.grad_data = torch.zeros_like(out)
+
+            def count(grad):
+                # Hacky way of recording the gradient data during the batch
+                # jacobian computation
+                self.weight.grad_data += grad
+
+            out.register_hook(count)
+        return out
+
+class ExpandingParameter(nn.Parameter):
+    def __new__(cls, data=None, convolutional=False, in_size=0, in_channels=0, requires_grad=True):
+        if data is None:
+            data = torch.Tensor()
+
+        instance = torch.Tensor._make_subclass(cls, data, requires_grad)
+        instance.input_data = None
+        instance.grad_data = None
+        instance.convolutional = convolutional
+        instance.listening = False
+        instance.in_channels = in_channels
+        instance.in_size = in_size
         return instance
 
     def expanded(self, meta_batch_size):
         return self.expand([meta_batch_size] + list(self.size())[1:])
 
-class BatchLinear(MetaModule):
+class ExpandingLinear(MetaModule):
 
     def __init__(self, input_size, output_size, bias=True):
         super(BatchLinear, self).__init__()
@@ -30,7 +104,6 @@ class BatchLinear(MetaModule):
         self.input_size = input_size
         self.output_size = output_size
         self.bias = bias
-        self.param_update = None
         if bias:
             input_size += 1
         self.weight = ExpandingParameter(torch.Tensor(1, self.output_size,
@@ -45,7 +118,7 @@ class BatchLinear(MetaModule):
             bound = 1 / math.sqrt(fan_in)
             init.uniform_(self.weight[:, :, -1], -bound, bound)
 
-    def forward(self, input_data, params=None):
+    def forward(self, input_data, params=None, save_warp_data=True):
         weight = self.weight.expanded(input_data.size(0)) if params is None else params["weight"]
 
         if self.bias:
@@ -61,25 +134,21 @@ class BatchLinear(MetaModule):
         else:
             out = torch.transpose(torch.bmm(weight, torch.transpose(input_data, 1,
                                                                         2)), 1, 2)
+        if save_warp_data:
+            self.enable_warp_data(input_data, out)
 
+        return out
 
+    def enable_warp_data(self, input_data, out):
         if self.weight.listening:
             # Save input data for future reference
-            if input_data.ndim > 3:
-                self.weight.input_data = input_data.mean(dim=-2)
-                self.weight.grad_data = torch.zeros_like(out.mean(dim=-2))
-            else:
-                self.weight.input_data = input_data
-                self.weight.grad_data = torch.zeros_like(out)
-
+            self.weight.input_data = input_data
+            self.weight.grad_data = torch.zeros_like(out)
 
             def count(grad):
                 # Hacky way of recording the gradient data during the batch
                 # jacobian computation
-                if grad.ndim > 3:
-                    self.weight.grad_data += grad.mean(dim=-2)
-                else:
-                    self.weight.grad_data += grad
+                self.weight.grad_data += grad
 
             out.register_hook(count)
         return out
@@ -142,10 +211,11 @@ def unfold(input, kernel_size, dilation=1, padding=0, stride=1):
     else:
         raise NotImplementedError("Input Error: Only 4D input Tensors are supported (got {}D)".format(input.dim()))
 
-class BatchConv2d(MetaModule):
+class BatchConv2d(BatchLinear):
 
-    def __init__(self, in_channels: int, out_channels: int, kernel_size, stride = 1, padding = 0, dilation = 1, groups: int = 1, bias: bool = True, padding_mode: str = 'zeros'):
-        super(BatchConv2d, self).__init__()
+    def __init__(self, in_channels: int, out_channels: int, kernel_size, stride = 1, padding = 0, dilation = 1, groups: int = 1, bias: bool = True, padding_mode: str = 'zeros', in_size=28, meta_batch_size=1):
+        # Call grandparent
+        super(BatchLinear, self).__init__()
 
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -157,7 +227,17 @@ class BatchConv2d(MetaModule):
         self.bias = bias
         self.padding_mode = padding_mode
 
-        self.linear = BatchLinear(self.in_channels * self.kernel_size[0] * self.kernel_size[1], out_channels, bias)
+        # self.linear = BatchLinear(self.in_channels * self.kernel_size[0] * self.kernel_size[1], out_channels, bias)
+
+        # Init BatchLinear
+        self.input_size = self.in_channels * self.kernel_size[0] * self.kernel_size[1]
+        self.output_size = out_channels
+        self.bias = bias
+        if bias:
+            self.input_size += 1
+        self.weight = BatchParameter(torch.Tensor(meta_batch_size, self.output_size,
+                                                 self.input_size), convolutional=True, in_size=in_size, in_channels=self.in_channels)
+        self.reset_parameters()
 
     def forward(self, inputs, params = None):
         meta_batch_size, batch_size, in_channels, in_h, in_w = inputs.size()
@@ -168,12 +248,16 @@ class BatchConv2d(MetaModule):
                 padding=self.padding, stride=self.stride).transpose(-1,-2)
         inp_unf = inp_unf.reshape(meta_batch_size, batch_size, *inp_unf.size()[-2:])
 
-        out_unf = self.linear(inp_unf, params=self.get_subdict(params, 'linear')).transpose(-1, -2)
+        # out_unf = self.linear(inp_unf, params=self.get_subdict(params, 'linear')).transpose(-1, -2)
+        out_unf = super().forward(inp_unf, params=params, save_warp_data=False).transpose(-1, -2)
 
-        out_h = math.floor((in_h + 2 * self.padding[0] - self.dilation[0] * (self.kernel_size[0] - 1) - 1)/self.stride[0] + 1)
-        out_w = math.floor((in_w + 2 * self.padding[1] - self.dilation[1] * (self.kernel_size[1] - 1) - 1)/self.stride[1] + 1)
+        self.out_h = math.floor((in_h + 2 * self.padding[0] - self.dilation[0] * (self.kernel_size[0] - 1) - 1)/self.stride[0] + 1)
+        self.out_w = math.floor((in_w + 2 * self.padding[1] - self.dilation[1] * (self.kernel_size[1] - 1) - 1)/self.stride[1] + 1)
 
-        out = out_unf.view(meta_batch_size, batch_size, self.out_channels, out_h, out_w)
+        out = out_unf.view(meta_batch_size, batch_size, self.out_channels, self.out_h, self.out_w)
+
+        self.enable_warp_data(inputs, out)
+
         return out
 
 class SquishMetaBatch(nn.Module):
@@ -195,11 +279,11 @@ class ExpandMetaBatch(nn.Module):
     def forward(self, inputs):
         return inputs.reshape(self.squish_meta_batch.meta_batch_size, -1, *inputs.size()[1:])
 
-def conv_block(in_channels, out_channels, **kwargs):
+def conv_block(in_channels, out_channels, in_size, **kwargs):
     squish = SquishMetaBatch()
     expand = ExpandMetaBatch(squish)
     return MetaSequential(OrderedDict([
-        ('conv', BatchConv2d(in_channels, out_channels, **kwargs)),
+        ('conv', BatchConv2d(in_channels, out_channels, in_size=in_size, **kwargs)),
         ('squish', squish),
         ('norm', nn.BatchNorm2d(out_channels, momentum=1.,
             track_running_stats=False)),
@@ -231,24 +315,28 @@ class MetaConvModel(MetaModule):
            for Fast Adaptation of Deep Networks. International Conference on
            Machine Learning (ICML) (https://arxiv.org/abs/1703.03400)
     """
-    def __init__(self, in_channels, out_features, hidden_size=64, feature_size=64):
+    def __init__(self, in_channels, out_features, hidden_size=64, feature_size=64, in_size=28, meta_batch_size=1):
         super(MetaConvModel, self).__init__()
         self.in_channels = in_channels
         self.out_features = out_features
         self.hidden_size = hidden_size
         self.feature_size = feature_size
 
+        in_sizes = [in_size]
+        for i in range(3):
+            in_sizes.append(math.floor(in_sizes[-1]/2))
+
         self.features = MetaSequential(OrderedDict([
             ('layer1', conv_block(in_channels, hidden_size, kernel_size=3,
-                                  stride=1, padding=1, bias=True)),
+                                  stride=1, padding=1, bias=True, in_size=in_sizes[0]**2, meta_batch_size=meta_batch_size)),
             ('layer2', conv_block(hidden_size, hidden_size, kernel_size=3,
-                                  stride=1, padding=1, bias=True)),
+                                  stride=1, padding=1, bias=True, in_size=in_sizes[1]**2, meta_batch_size=meta_batch_size)),
             ('layer3', conv_block(hidden_size, hidden_size, kernel_size=3,
-                                  stride=1, padding=1, bias=True)),
+                                  stride=1, padding=1, bias=True, in_size=in_sizes[2]**2, meta_batch_size=meta_batch_size)),
             ('layer4', conv_block(hidden_size, hidden_size, kernel_size=3,
-                                  stride=1, padding=1, bias=True))
+                                  stride=1, padding=1, bias=True, in_size=in_sizes[3]**2, meta_batch_size=meta_batch_size))
         ]))
-        self.classifier = BatchLinear(feature_size, out_features, bias=True)
+        self.classifier = BatchLinear(feature_size, out_features, bias=True, meta_batch_size=meta_batch_size)
 
     def forward(self, inputs, params=None):
         features = self.features(inputs, params=self.get_subdict(params, 'features'))
@@ -277,7 +365,7 @@ class MetaMLPModel(MetaModule):
            for Fast Adaptation of Deep Networks. International Conference on
            Machine Learning (ICML) (https://arxiv.org/abs/1703.03400)
     """
-    def __init__(self, in_features, out_features, hidden_sizes):
+    def __init__(self, in_features, out_features, hidden_sizes, meta_batch_size=1):
         super(MetaMLPModel, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
@@ -287,26 +375,26 @@ class MetaMLPModel(MetaModule):
 
         self.features = MetaSequential(OrderedDict([('layer{0}'.format(i + 1),
             MetaSequential(OrderedDict([
-                ('linear', BatchLinear(hidden_size, layer_sizes[i + 1], bias=True)),
+                ('linear', BatchLinear(hidden_size, layer_sizes[i + 1], meta_batch_size=meta_batch_size, bias=True)),
                 ('relu', nn.ReLU())
             ]))) for (i, hidden_size) in enumerate(layer_sizes[:-1])]))
-        self.classifier = BatchLinear(hidden_sizes[-1], out_features, bias=True)
+        self.classifier = BatchLinear(hidden_sizes[-1], out_features, meta_batch_size=meta_batch_size, bias=True)
 
     def forward(self, inputs, params=None):
         features = self.features(inputs, params=self.get_subdict(params, 'features'))
         logits = self.classifier(features, params=self.get_subdict(params, 'classifier'))
         return logits
 
-def ModelConvOmniglot(out_features, hidden_size=64):
+def ModelConvOmniglot(out_features, hidden_size=64, meta_batch_size=1):
     return MetaConvModel(1, out_features, hidden_size=hidden_size,
-                         feature_size=hidden_size)
+                         feature_size=hidden_size, meta_batch_size=meta_batch_size)
 
-def ModelConvMiniImagenet(out_features, hidden_size=64):
+def ModelConvMiniImagenet(out_features, hidden_size=64, meta_batch_size=1):
     return MetaConvModel(3, out_features, hidden_size=hidden_size,
-                         feature_size=5 * 5 * hidden_size)
+                         feature_size=5 * 5 * hidden_size, in_size=84, meta_batch_size=meta_batch_size)
 
-def ModelMLPSinusoid(hidden_sizes=[40, 40]):
-    return MetaMLPModel(1, 1, hidden_sizes)
+def ModelMLPSinusoid(hidden_sizes=[40, 40], meta_batch_size=1):
+    return MetaMLPModel(1, 1, hidden_sizes, meta_batch_size=meta_batch_size)
 
 if __name__ == '__main__':
     model = ModelMLPSinusoid()

maml/transformer_metric.py
@@ -1,17 +1,41 @@
 import torch
 from torch import nn
 from typing import List
-from math import sqrt
+from math import sqrt, ceil
 from .model import ExpandingParameter
 from torch.nn.init import orthogonal_
+from torch.nn.modules.normalization import LayerNorm
+
+class MiniConvBlock(nn.Module):
+
+    def __init__(self, in_size, in_channels, out_channels, out_flat, **kwargs):
+        super(MiniConvBlock, self).__init__()
+
+        self.conv = nn.Conv2d(in_channels, out_channels, **kwargs)
+        self.norm = nn.BatchNorm2d(out_channels, momentum=1,
+                track_running_stats=False)
+        self.relu = nn.ReLU()
+        self.linear = nn.Linear(in_size * out_channels, out_flat, bias=True)
+
+    def forward(self, input_data):
+        batch_sizes = input_data.size()[:2]
+        out = input_data.reshape(-1, *input_data.size()[2:])
+        out = self.conv(out)
+        out = self.norm(out)
+        out = self.relu(out)
+        out = out.reshape(*batch_sizes, -1)
+        out = self.linear(out)
+        out = self.relu(out)
+        return out
+
 
 class TransformerMetric(nn.Module):
 
-    def __init__(self, warp_parameters, hidden_size: int = 16, transformer_layers: int = 2, transformer_heads: int = 4, transformer_feedforward_dim: int = 32, transformer_d_model: int = 16, transformer_cascade_direction: str = "bidirectional", num_outer_products = 5):
+    def __init__(self, warp_parameters, hidden_size: int = 512, transformer_layers: int = 4, transformer_heads: int = 8, transformer_feedforward_dim: int = 2048, transformer_d_model: int = 512, transformer_cascade_direction: str = "bidirectional", num_outer_products = 32, conv_hidden_dim: int = 2048):
 
         super(TransformerMetric, self).__init__()
 
-        self.linear_input = []
+        self.input_modules = []
         self.warp_parameters = warp_parameters
 
         self.hidden_size = hidden_size
@@ -21,18 +45,37 @@ class TransformerMetric(nn.Module):
         self.transformer_d_model = transformer_d_model
         self.transformer_cascade_direction = transformer_cascade_direction
         self.num_outer_products = num_outer_products
+        self.conv_hidden_dim = conv_hidden_dim
 
         for param in warp_parameters:
-            input_dim = param.size(2) + param.size(1)
-            self.linear_input.append(nn.Linear(input_dim,
-                                               self.transformer_d_model))
-        self.linear_input = nn.ModuleList(self.linear_input)
+            if param.convolutional:
+                hidden_dim = ceil(conv_hidden_dim / param.in_size)
+
+                mini_conv_block_1 = MiniConvBlock(param.in_size, param.in_channels, hidden_dim, self.transformer_d_model // 2, kernel_size=3, stride=1, padding=1, bias=True)
+                mini_conv_block_2 = MiniConvBlock(param.in_size, param.size(-2), hidden_dim, self.transformer_d_model // 2, kernel_size=3, stride=1, padding=1, bias=True)
+                self.input_modules.append(nn.ModuleList([mini_conv_block_1, mini_conv_block_2]))
+            else:
+                input_dim = param.size(2) + param.size(1)
+                self.input_modules.append(nn.Linear(input_dim,
+                                                self.transformer_d_model))
+        self.input_modules = nn.ModuleList(self.input_modules)
         direction = self.transformer_cascade_direction
+
+
+        self.layer_norms = []
+        for i in range(self.transformer_layers):
+            self.layer_norms.append([])
+            for j in range(len(warp_parameters)):
+                self.layer_norms[-1].append(LayerNorm(self.transformer_d_model))
+
+        self.layer_norms = nn.ModuleList([nn.ModuleList(self.layer_norms[i]) for i in range(self.transformer_layers)])
+
 
         if direction == "forward" or direction == "bidirectional":
             self.forward_transformers = [[] for _ in range(self.transformer_layers)]
             for i in range(self.transformer_layers):
                 for j in range(len(warp_parameters)):
+                    """
                     if j == 0:
                         self.forward_transformers[i].append(
                             nn.TransformerEncoderLayer(self.transformer_d_model,
@@ -41,12 +84,13 @@ class TransformerMetric(nn.Module):
                                                       dropout=0)
                         )
                     else:
-                        self.forward_transformers[i].append(
-                            nn.TransformerDecoderLayer(self.transformer_d_model,
-                                                      self.transformer_heads,
-                                                      self.transformer_feedforward_dim,
-                                                      dropout=0)
-                        )
+                    """
+                    self.forward_transformers[i].append(
+                        nn.TransformerDecoderLayer(self.transformer_d_model,
+                                                    self.transformer_heads,
+                                                    self.transformer_feedforward_dim,
+                                                    dropout=0)
+                    )
 
             self.forward_transformers = nn.ModuleList([nn.ModuleList(transformers) for transformers in self.forward_transformers])
 
@@ -55,6 +99,7 @@ class TransformerMetric(nn.Module):
                                          range(self.transformer_layers)]
             for i in range(self.transformer_layers):
                 for j in range(len(warp_parameters)):
+                    """
                     if j == len(warp_parameters) - 1:
                         self.backward_transformers[i].append(
                             nn.TransformerEncoderLayer(self.transformer_d_model,
@@ -63,12 +108,13 @@ class TransformerMetric(nn.Module):
                                                       dropout=0)
                         )
                     else:
-                        self.backward_transformers[i].append(
-                            nn.TransformerDecoderLayer(self.transformer_d_model,
-                                                      self.transformer_heads,
-                                                      self.transformer_feedforward_dim,
-                                                      dropout=0)
-                        )
+                    """
+                    self.backward_transformers[i].append(
+                        nn.TransformerDecoderLayer(self.transformer_d_model,
+                                                    self.transformer_heads,
+                                                    self.transformer_feedforward_dim,
+                                                    dropout=0)
+                    )
             self.backward_transformers = nn.ModuleList([nn.ModuleList(transformers) for transformers in self.backward_transformers])
 
         if direction == "bidirectional":
@@ -95,6 +141,10 @@ class TransformerMetric(nn.Module):
                             param.size(2)),
                     nn.Linear(self.transformer_d_model,
                               self.num_outer_products),
+                    nn.Linear(self.transformer_d_model,
+                              1),
+                    nn.Linear(self.transformer_d_model,
+                              1)
                 ]
             )
 
@@ -115,14 +165,33 @@ class TransformerMetric(nn.Module):
                 self.output_heads[-1][3].bias /= self.num_outer_products
         self.output_heads = nn.ModuleList([nn.ModuleList(output_head) for output_head in self.output_heads])
 
-    def forward(self, warp_inputs: List[List[torch.Tensor]]):
+    def forward(self, warp_inputs: List[List[torch.Tensor]], state: List[List[torch.Tensor]] = None):
         embeddings = []
         direction = self.transformer_cascade_direction
 
         # Get all inputs to be same dimension so we can pass through the
         # transformer cascade
         for i, warp_input in enumerate(warp_inputs):
-            embeddings.append(self.linear_input[i](torch.cat(warp_input, dim=-1)))
+            if self.warp_parameters[i].convolutional:
+                embeddings_1 = self.input_modules[i][0](warp_input[0])
+                embeddings_2 = self.input_modules[i][1](warp_input[1])
+                embeddings.append(torch.cat([embeddings_1, embeddings_2], dim=-1))
+            else:
+                embeddings.append(self.input_modules[i](torch.cat(warp_input, dim=-1)))
+
+        if state is None:
+            state = [None, None]
+            if direction == "forward" or direction == "bidirectional":
+                state[0] = []
+                for i in range(self.transformer_layers):
+                    state_tensor = torch.zeros_like(embeddings[0].transpose(0,1))
+                    state[0].append(state_tensor)
+
+            if direction == "backward" or direction == "bidirectional":
+                state[1] = []
+                for i in range(self.transformer_layers):
+                    state_tensor = torch.zeros_like(embeddings[0].transpose(0,1))
+                    state[1].append(state_tensor)
 
         for i in range(self.transformer_layers):
             if direction == "forward" or direction == "bidirectional":
@@ -130,11 +199,14 @@ class TransformerMetric(nn.Module):
                 memory = None
                 for j, embedding in enumerate(embeddings):
                     if j == 0:
-                        memory = \
-                                self.forward_transformers[i][j](embedding.transpose(0,1))
+                        memory = self.forward_transformers[i][j](
+                            embedding.transpose(0,1), state[0][i])
                     else:
                         memory = self.forward_transformers[i][j](
                             embedding.transpose(0,1), memory)
+
+                    if j == len(embeddings) - 1:
+                        state[0][i] = memory
 
                     forward_transformer_out.append(memory)
                 forward_transformer_out = torch.stack(forward_transformer_out)
@@ -144,11 +216,14 @@ class TransformerMetric(nn.Module):
                 memory = None
                 for j, embedding in reversed(list(enumerate(embeddings))):
                     if j == len(embeddings) - 1:
-                        memory = \
-                                self.backward_transformers[i][j](embedding.transpose(0,1))
+                        memory = self.backward_transformers[i][j](
+                            embedding.transpose(0,1), state[1][i])
                     else:
                         memory = self.backward_transformers[i][j](
                             embedding.transpose(0,1), memory)
+
+                    if j == 0:
+                        state[1][i] = memory
 
                     backward_transformer_out.insert(0, memory)
                 backward_transformer_out = torch.stack(backward_transformer_out)
@@ -165,6 +240,9 @@ class TransformerMetric(nn.Module):
             if direction == "backward":
                 embeddings = backward_transformer_out.transpose(1,2)
 
+            for j in range(len(embeddings)):
+                embeddings[i][j] = self.layer_norms[i][j](embeddings[i][j])
+
         output_matrices = []
         for i, (embedding, param) in enumerate(zip(embeddings,
                                                    self.warp_parameters)):
@@ -172,6 +250,9 @@ class TransformerMetric(nn.Module):
             matrix_b_scalar = self.output_heads[i][1](embedding)
             matrix_a_gen = self.output_heads[i][2](embedding)
             matrix_a_scalar = self.output_heads[i][3](embedding)
+
+            matrix_b_eye = self.output_heads[i][4](embedding)
+            matrix_a_eye = self.output_heads[i][5](embedding)
 
             original_shape_b = matrix_b_gen.size()
             new_shape_b = [-1, param.size(1)]
@@ -186,6 +267,10 @@ class TransformerMetric(nn.Module):
                              [self.num_outer_products] + 2 * [param.size(1)])
             matrix_b = matrix_b.sum(dim=-3)
 
+            matrix_b += matrix_b_eye.unsqueeze(-1) * \
+                    torch.eye(matrix_b.size(-1), device=matrix_b.device
+                            ).unsqueeze(0).unsqueeze(0).expand_as(matrix_b)
+
             original_shape_a = matrix_a_gen.size()
             new_shape_a = [-1, param.size(2)]
             matrix_a_gen = matrix_a_gen.reshape(new_shape_a)
@@ -197,9 +282,13 @@ class TransformerMetric(nn.Module):
                              [self.num_outer_products] + 2 * [param.size(2)])
             matrix_a = matrix_a.sum(dim=-3)
 
+            matrix_a += matrix_a_eye.unsqueeze(-1) * \
+                    torch.eye(matrix_a.size(-1), device=matrix_a.device
+                            ).unsqueeze(0).unsqueeze(0).expand_as(matrix_a)
+
             output_matrices.append([matrix_a, matrix_b])
 
-        return output_matrices
+        return output_matrices, state
 
     def set_listening(self, listening: bool):
         for param in self.warp_parameters:

maml/utils.py
@@ -2,7 +2,7 @@ import torch
 
 from collections import OrderedDict
 from torchmeta.modules import MetaModule
-from .model import BatchLinear
+from .model import BatchLinear, ExpandingLinear
 from .transformer_metric import TransformerMetric
 
 from .expm import torch_expm as expm
@@ -10,8 +10,12 @@ from .expm import torch_expm as expm
 def compute_accuracy(logits, targets):
     """Compute the accuracy"""
     with torch.no_grad():
-        _, predictions = torch.max(logits, dim=2)
-        accuracy = torch.mean(predictions.eq(targets).float(), dim=-1)
+        if logits.dim() == 2:
+            _, predictions = torch.max(logits, dim=1)
+            accuracy = torch.mean(predictions.eq(targets).float())
+        else:
+            _, predictions = torch.max(logits, dim=2)
+            accuracy = torch.mean(predictions.eq(targets).float(), dim=-1)
     return accuracy.detach().cpu().numpy()
 
 def tensors_to_device(tensors, device=torch.device('cpu')):
@@ -42,7 +46,7 @@ class ToTensor1D(object):
 def make_warp_model(model):
     metric_params = []
     for layer in model.modules():
-        if isinstance(layer, BatchLinear):
+        if isinstance(layer, BatchLinear) or isinstance(layer, ExpandingLinear):
             metric_params.append(layer.weight)
 
     return TransformerMetric(metric_params)
@@ -77,7 +81,8 @@ def gradient_update_parameters_warp(model,
                                params=None,
                                warp_model=None,
                                step_size=0.5,
-                               first_order=False):
+                               first_order=False,
+                               state=None):
     """Update of the meta-parameters with one step of gradient descent on the
     loss function.
     Parameters
@@ -109,12 +114,10 @@ def gradient_update_parameters_warp(model,
     if params is None:
         params = OrderedDict(model.meta_named_parameters())
 
-
     param_jacobs_lst = [[] for _ in range(len(params))]
     for i in range(loss.size(0)):
         grads = torch.autograd.grad(loss[i], params.values(), retain_graph=True, create_graph=not first_order)
         for j, grad in enumerate(grads):
-            #param_jacobs[j][:, i, :, :] += grad
             param_jacobs_lst[j].append(grad)
 
     param_jacobs = [torch.stack(param_jacob, dim=1) for param_jacob in param_jacobs_lst]
@@ -122,9 +125,9 @@ def gradient_update_parameters_warp(model,
     if warp_model is not None:
         warp_model_input = []
         for param in warp_model.warp_parameters:
-            warp_model_input.append([param.input_data.detach(), param.grad_data.detach()])
+            warp_model_input.append([param.input_data, param.grad_data])
 
-        kronecker_matrix_logs = warp_model(warp_model_input)
+        kronecker_matrix_logs, state = warp_model(warp_model_input, state=state)
         kronecker_matrices = []
 
         for matrix_a, matrix_b in kronecker_matrix_logs:
@@ -157,4 +160,4 @@ def gradient_update_parameters_warp(model,
                 grad = kronecker_warp(grad, kronecker_matrices[i])
             updated_params[name] = param - step_size * grad.mean(dim=-3)
 
-    return updated_params
+    return updated_params, state

train.py
@@ -12,6 +12,94 @@ from maml.metalearners import ModelAgnosticMetaLearning
 from tqdm import tqdm
 from maml.utils import make_warp_model
 import wandb
+from torch.optim.lr_scheduler import LambdaLR
+
+def get_linear_schedule_with_warmup(optimizer, num_warmup_steps, num_training_steps, last_epoch=-1):
+    """
+    Create a schedule with a learning rate that decreases linearly from the initial lr set in the optimizer to 0,
+    after a warmup period during which it increases linearly from 0 to the initial lr set in the optimizer.
+
+    Args:
+        optimizer (:class:`~torch.optim.Optimizer`):
+            The optimizer for which to schedule the learning rate.
+        num_warmup_steps (:obj:`int`):
+            The number of steps for the warmup phase.
+        num_training_steps (:obj:`int`):
+            The total number of training steps.
+        last_epoch (:obj:`int`, `optional`, defaults to -1):
+            The index of the last epoch when resuming training.
+
+    Return:
+        :obj:`torch.optim.lr_scheduler.LambdaLR` with the appropriate schedule.
+    """
+
+    def lr_lambda(current_step: int):
+        if current_step < num_warmup_steps:
+            return float(current_step) / float(max(1, num_warmup_steps))
+        return max(
+            0.0, (float(num_training_steps - current_step) / float(max(1, num_training_steps - num_warmup_steps))) # * (0.5 + 0.5 * math.cos(- math.pi + 2 * math.pi * current_step / wavelength))
+        )
+
+    return LambdaLR(optimizer, lr_lambda, last_epoch)
+
+
+def get_cosine_schedule(optimizer, wavelength=100, last_epoch=-1):
+    """
+    Create a schedule with a learning rate that decreases linearly from the initial lr set in the optimizer to 0,
+    after a warmup period during which it increases linearly from 0 to the initial lr set in the optimizer.
+
+    Args:
+        optimizer (:class:`~torch.optim.Optimizer`):
+            The optimizer for which to schedule the learning rate.
+        num_warmup_steps (:obj:`int`):
+            The number of steps for the warmup phase.
+        num_training_steps (:obj:`int`):
+            The total number of training steps.
+        last_epoch (:obj:`int`, `optional`, defaults to -1):
+            The index of the last epoch when resuming training.
+
+    Return:
+        :obj:`torch.optim.lr_scheduler.LambdaLR` with the appropriate schedule.
+    """
+
+    def lr_lambda(current_step: int):
+        return max(0.0, (0.5 + 0.5 * math.cos(2 * math.pi * current_step / wavelength)))
+
+    return LambdaLR(optimizer, lr_lambda, last_epoch)
+
+def get_cosine_with_hard_restarts_schedule_with_warmup(
+    optimizer, num_warmup_steps: int, num_training_steps: int, num_cycles: int = 1, last_epoch: int = -1
+):
+    """
+    Create a schedule with a learning rate that decreases following the values of the cosine function between the
+    initial lr set in the optimizer to 0, with several hard restarts, after a warmup period during which it increases
+    linearly between 0 and the initial lr set in the optimizer.
+
+    Args:
+        optimizer (:class:`~torch.optim.Optimizer`):
+            The optimizer for which to schedule the learning rate.
+        num_warmup_steps (:obj:`int`):
+            The number of steps for the warmup phase.
+        num_training_steps (:obj:`int`):
+            The total number of training steps.
+        num_cycles (:obj:`int`, `optional`, defaults to 1):
+            The number of hard restarts to use.
+        last_epoch (:obj:`int`, `optional`, defaults to -1):
+            The index of the last epoch when resuming training.
+
+    Return:
+        :obj:`torch.optim.lr_scheduler.LambdaLR` with the appropriate schedule.
+    """
+
+    def lr_lambda(current_step):
+        if current_step < num_warmup_steps:
+            return float(current_step) / float(max(1, num_warmup_steps))
+        progress = float(current_step - num_warmup_steps) / float(max(1, num_training_steps - num_warmup_steps))
+        if progress >= 1.0:
+            return 0.0
+        return max(0.0, 0.5 * (1.0 + math.cos(math.pi * ((float(num_cycles) * progress) % 1.0))))
+
+    return LambdaLR(optimizer, lr_lambda, last_epoch)
 
 def main(args):
     logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
@@ -43,7 +131,8 @@ def main(args):
                                       args.num_ways,
                                       args.num_shots,
                                       args.num_shots_test,
-                                      hidden_size=args.hidden_size)
+                                      hidden_size=args.hidden_size,
+                                      meta_batch_size=args.batch_size)
 
     meta_train_dataloader = BatchMetaDataLoader(benchmark.meta_train_dataset,
                                                 batch_size=args.batch_size,
@@ -59,17 +148,30 @@ def main(args):
     warp_model = None
     if args.warp:
         warp_model = make_warp_model(benchmark.model)
-        meta_optimizer = torch.optim.Adam(list(warp_model.parameters()) + list(benchmark.model.parameters()), lr=args.meta_lr)
+        meta_optimizer = torch.optim.Adam(benchmark.model.parameters(), lr=args.meta_lr)
+        warp_meta_optimizer = torch.optim.Adam(warp_model.parameters(), lr=args.warp_lr)
+
+        #warp_meta_optimizer = None
+        #meta_optimizer = torch.optim.Adam(warp_model.parameters(), lr=args.warp_lr)
     else:
         meta_optimizer = torch.optim.Adam(benchmark.model.parameters(), lr=args.meta_lr)
+        warp_meta_optimizer = None
+
+    warp_scheduler = get_linear_schedule_with_warmup(warp_meta_optimizer, 500, 100000, last_epoch=-1)
+    scheduler = get_linear_schedule_with_warmup(meta_optimizer, 500, 100000, last_epoch=-1)
+
     metalearner = ModelAgnosticMetaLearning(benchmark.model,
                                             meta_optimizer,
+                                            scheduler=scheduler,
+                                            warp_optimizer=warp_meta_optimizer,
                                             warp_model=warp_model,
+                                            warp_scheduler=warp_scheduler,
                                             first_order=args.first_order,
                                             num_adaptation_steps=args.num_steps,
                                             step_size=args.step_size,
                                             loss_function=benchmark.loss_function,
-                                            device=device)
+                                            device=device,
+                                            num_maml_steps=args.num_maml_steps)
 
     best_value = None
 
@@ -82,7 +184,7 @@ def main(args):
                           desc='Training',
                           leave=False)
         results = metalearner.evaluate(meta_val_dataloader,
-                                       max_batches=args.num_batches,
+                                       max_batches=args.num_eval_batches,
                                        verbose=args.verbose,
                                        desc=epoch_desc.format(epoch + 1))
 
@@ -138,19 +240,26 @@ if __name__ == '__main__':
     parser.add_argument('--num-steps', type=int, default=1,
         help='Number of fast adaptation steps, ie. gradient descent '
         'updates (default: 1).')
-    parser.add_argument('--num-epochs', type=int, default=50,
+    parser.add_argument('--num-epochs', type=int, default=200,
         help='Number of epochs of meta-training (default: 50).')
-    parser.add_argument('--num-batches', type=int, default=100,
+    parser.add_argument('--num-batches', type=int, default=500,
         help='Number of batch of tasks per epoch (default: 100).')
+    parser.add_argument('--num-eval-batches', type=int, default=100)
     parser.add_argument('--step-size', type=float, default=0.1,
         help='Size of the fast adaptation step, ie. learning rate in the '
         'gradient descent update (default: 0.1).')
     parser.add_argument('--first-order', action='store_true',
         help='Use the first order approximation, do not use higher-order '
         'derivatives during meta-optimization.')
-    parser.add_argument('--meta-lr', type=float, default=0.001,
+    parser.add_argument('--meta-lr', type=float, default=0.0001,
         help='Learning rate for the meta-optimizer (optimization of the outer '
-        'loss). The default optimizer is Adam (default: 1e-3).')
+        'loss). The default optimizer is Adam (default: 1e-4).')
+    parser.add_argument('--warp-lr', type=float, default=0.00001,
+        help='Learning rate for the meta-optimizer (optimization of the outer '
+        'loss). The default optimizer is Adam (default: 1e-5).')
+    parser.add_argument('--num-maml-steps', type=int, default=0,
+        help='Number of steps to update initialization for before freezing it')
+    parser.add_argument
 
     # Misc
     parser.add_argument('--num-workers', type=int, default=1,